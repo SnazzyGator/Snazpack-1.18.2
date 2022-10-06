ccfa900b75f317d5de17e106fa2524d29268450f
SnazzyGator <snazzygator@gmail.com>
2022-10-05 23:39:44 -0400

    crafthelper hell

--- src/main/java/net/snazzygator/snazpackmod/recipe/ArtisanStationRecipe.java
+++ src/main/java/net/snazzygator/snazpackmod/recipe/ArtisanStationRecipe.java
@@ -1,29 +1,41 @@
 package net.snazzygator.snazpackmod.recipe;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
+import com.google.common.collect.Lists;
+import com.google.gson.*;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.TagParser;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.crafting.CompoundIngredient;
+import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.common.crafting.IIngredientSerializer;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.snazzygator.snazpackmod.SnazpackMod;
 
 import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Objects;
 
 public class ArtisanStationRecipe implements Recipe<SimpleContainer> {
     private final ResourceLocation id;
     private final ItemStack output;
     private final NonNullList<Ingredient> recipeItems;
+    private static Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
 
-    public ArtisanStationRecipe(ResourceLocation id, ItemStack output,
+    public ArtisanStationRecipe(ResourceLocation id, ItemStack itemstack,
                                 NonNullList<Ingredient> recipeItems) {
         this.id = id;
-        this.output = output;
+        this.output = itemstack;
         this.recipeItems = recipeItems;
     }
 
@@ -31,6 +43,15 @@ public class ArtisanStationRecipe implements Recipe<SimpleContainer> {
     public boolean matches(SimpleContainer pContainer, Level pLevel) {
         return recipeItems.get(0).test(pContainer.getItem(0))
                 && recipeItems.get(1).test(pContainer.getItem(1));
+    }
+
+
+    public static ItemStack itemStackFromJson(JsonObject pStackObject) {
+        return getItemStack(pStackObject, true, true);
+    }
+    public static ItemStack getItemStack(JsonObject json, boolean readNBT)
+    {
+        return getItemStack(json, readNBT, false);
     }
 
     @Override
@@ -75,7 +96,7 @@ public class ArtisanStationRecipe implements Recipe<SimpleContainer> {
 
         @Override
         public ArtisanStationRecipe fromJson(ResourceLocation id, JsonObject json) {
-            ItemStack output = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(json, "output"));
+            ItemStack itemstack = itemStackFromJson(GsonHelper.getAsJsonObject(json, "output"));
 
             JsonArray ingredients = GsonHelper.getAsJsonArray(json, "ingredients");
             NonNullList<Ingredient> inputs = NonNullList.withSize(2, Ingredient.EMPTY);
@@ -84,7 +105,7 @@ public class ArtisanStationRecipe implements Recipe<SimpleContainer> {
                 inputs.set(i, Ingredient.fromJson(ingredients.get(i)));
             }
 
-            return new ArtisanStationRecipe(id, output, inputs);
+            return new ArtisanStationRecipe(id, itemstack, inputs);
         }
 
         @Override
@@ -95,8 +116,8 @@ public class ArtisanStationRecipe implements Recipe<SimpleContainer> {
                 inputs.set(i, Ingredient.fromNetwork(buf));
             }
 
-            ItemStack output = buf.readItem();
-            return new ArtisanStationRecipe(id, output, inputs);
+            ItemStack itemstack = buf.readItem();
+            return new ArtisanStationRecipe(id, itemstack, inputs);
         }
 
         @Override
@@ -129,4 +150,57 @@ public class ArtisanStationRecipe implements Recipe<SimpleContainer> {
             return (Class<G>)cls;
         }
     }
+    
+    // crafthelper stuff, dont even know if necessary but it's here
+    
+    public static ItemStack getItemStack(JsonObject json, boolean readNBT, boolean disallowsAirInRecipe)
+    {
+        String itemName = GsonHelper.getAsString(json, "item");
+        Item item = getItem(itemName, disallowsAirInRecipe);
+        if (readNBT && json.has("nbt"))
+        {
+            CompoundTag nbt = getNBT(json.get("nbt"));
+            CompoundTag tmp = new CompoundTag();
+            if (nbt.contains("ForgeCaps"))
+            {
+                tmp.put("ForgeCaps", nbt.get("ForgeCaps"));
+                nbt.remove("ForgeCaps");
+            }
+
+            tmp.put("tag", nbt);
+            tmp.putString("id", itemName);
+            tmp.putInt("Count", GsonHelper.getAsInt(json, "count", 1));
+
+            return ItemStack.of(tmp);
+        }
+
+        return new ItemStack(item, GsonHelper.getAsInt(json, "count", 1));
+    }
+    public static Item getItem(String itemName, boolean disallowsAirInRecipe)
+    {
+        ResourceLocation itemKey = new ResourceLocation(itemName);
+        if (!ForgeRegistries.ITEMS.containsKey(itemKey))
+            throw new JsonSyntaxException("Unknown item '" + itemName + "'");
+
+        Item item = ForgeRegistries.ITEMS.getValue(itemKey);
+        if (disallowsAirInRecipe && item == Items.AIR)
+            throw new JsonSyntaxException("Invalid item: " + itemName);
+        return Objects.requireNonNull(item);
+    }
+
+    public static CompoundTag getNBT(JsonElement element)
+    {
+        try
+        {
+            if (element.isJsonObject())
+                return TagParser.parseTag(GSON.toJson(element));
+            else
+                return TagParser.parseTag(GsonHelper.convertToString(element, "nbt"));
+        }
+        catch (CommandSyntaxException e)
+        {
+            throw new JsonSyntaxException("Invalid NBT Entry: " + e);
+        }
+    }
+
 }
